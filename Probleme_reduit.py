679fb03a9249629b95c7230aa6935112b401c9e4
GabrielJB <45339507+GabrielJB@users.noreply.github.com>
2018-12-28 16:17:20 +0100

    Update Probleme_reduit.py

--- Probleme_reduit.py
+++ Probleme_reduit.py
@@ -3,7 +3,7 @@
 ## Circuits et Parametres
 map = [[0, 0, 0, -1, 0, 0, 0, 0, 0, 0, 0], [0, 0, 1, 1, 1, 0, 0, 0, 0, 0, 0], [0, 0, 2, 2, 2, 2, 0, 0, 0, 0, 0], [0, 0, 0, 3, 3, 3, 3, 0, 0, 0, 0], [0, 0, 0, 4, 4, 4, 4, 0, 0, 0, 0], [0, 0, 0, 0, 5, 5, 5, 5, 5, 0, 0], [0, 0, 0, 0, 0, 6, 6, 6, 6, 6, 0], [0, 0, 0, 7, 7, 7, 7, 7, 7, 0, 0], [0, 8, 8, 8, 8, 8, 8, 8, 0, 0, 0], [0, 9, 9, 9, 9, 9, 9, 0, 0, 0, 0], [0, 10, 10, 10, 10, 0, 0, 0, 0, 0, 0]]
 
-N = len(map) # map est une matrice caree
+# map est une matrice caree
 tableau = [[[0 for j in range(2)] for k in range(N)] for l in range(N)]
 
 def print_map(map):
@@ -37,15 +37,21 @@ def vitesse_compatible(v1, v2): #teste si en arrivant dans une case a v1, en la 
                 return True
     return False
 
+def cases_accessible()
+
 def trajets_possibles(x_dep, y_dep, x_arr): #y_arr = N - 1
-    #renvoie la liste des listes des case intermediaires, nombre de coups (nombre de cases intermediaires), vitesse a avoir en quittant la case
+    #renvoie la liste des 3-uplets:
+        #listes des case intermediaires,
+        #nombre de coups (nombre de cases intermediaires),
+        #vitesse a avoir en quittant la case
+    traj_poss = []
     
-    
-#On veut que n ( = ligne sur laquelle se trouve la voiture ) soit N - 1 = 10 ici.
-#On trouvera les chemins et nb de coups necessaires pour chaque case d'arrivee. On en prend le min.
+#On trouvera les chemins et nb de coups necessaires pour arriver a chaque case d'arrivee.
+#On en prend le min.
 
 #On part de la dernière ligne et on remonte jusqu'a la premiere
-def ...(map):
+def plus_courts_chemin(map):
+    N = len(map)
     nb_ligne_arr = [i for i in range(N) if map[N - 1][i] != 0]
     for nb_ligne in range(N - 1, -1, -1):
         #A chaque ligne, on determine pour chaque case du circuit (case !=0) en combien de coups on peut atteindre une case N.
@@ -54,7 +60,8 @@ def ...(map):
         for nb_colonne in range(N):
             case = ligne[nb_colonne]
             if case != 0:
-                #On associe a cette case la liste des trajets possibles pour atteindre une case N avec la vitesse a avoir en quittant cette case
+                #On associe a cette case la liste des trajets possibles
+                #pour atteindre une case N avec la vitesse a avoir en quittant cette case
                 li_traj_poss = []
                 for i in nb_ligne_arr:
                     li += trajets_possibles( nb_ligne, nb_colonne, i)
